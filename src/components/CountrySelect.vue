36347bc697e1154eeba2948b131c8c226d3ac092
dezoliveira <andresoliveira@protonmail.com>
2021-11-29 22:16:24 -0300

    Ajustes no Layout

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <label class="flex justify-center form-select">Selecione um País:</label>
+  <div class="flex justify-center align-center">
+    <select @change="onChange()" 
+      v-model="selected" 
+      class="text-center form-select bg-white w-80 mb-10 block border p-3 rounded  focus:outline-none focus:ring-2 focus:ring-green-600 shadow-md"
+      >
+<option value="0">--</option>
+      <option 
+        v-for="country in countries"
+        :value="country.ID"
+      >
+        {{country.Country}}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountrySelect',
+
+  props: ['countries'],
+  
+  data(){
+    return {
+      selected: 0
+    }
+  },
+
+  methods:{
+    onChange() {
+
+      const country = this.countries.find((item) => item.ID === this.selected)
+      this.$emit('get-country', country)
+    },
+  }
+
+}
+</script>
---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <!-- Box 1 -->
+    <div class="shadow-md bg-green-500 text-white p-10 text-center rounded d-flex justify-content-center">
+      <i class="fas fa-heartbeat text-3xl"></i>
+      <h3 class="text-3xl text-white-900 font-bold mb-4">
+        Casos
+      </h3>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Novos:</span>
+        {{numberWithCommas(stats.NewConfirmed)}}
+      </div>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{numberWithCommas(stats.TotalConfirmed)}}
+      </div>
+
+    </div>
+
+    <!-- Box 2 -->
+    <div class="shadow-md bg-red-500 text-white p-10 text-center rounded d-flex justify-content-center">
+      <i class="fas fa-procedures text-3xl"></i>
+      <h3 class="text-3xl text-white-900 font-bold mb-4">
+        Mortos
+      </h3>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Novos:</span>
+        {{numberWithCommas(stats.NewDeaths)}}
+      </div>
+
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{numberWithCommas(stats.TotalDeaths)}}
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'DataBoxes',
+  props: ['stats'],
+  methods: {
+    numberWithCommas(x){
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    }
+  }
+}
+</script>
